8b08f3893c8d03acb59059421971608440ceab3c
facundomarcos <facundomarcos@live.com.ar>
2022-09-22 20:14:30 -0300

    33

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -11,18 +11,30 @@ namespace BulkyBookWeb.Controllers
         {
             _db = db;
         }
+        //GET
         public IActionResult Index()
         {
             IEnumerable<Category> objCategoryList = _db.Categories;
 
             return View(objCategoryList);
         }
-
+        //GET
         public IActionResult Create()
         {
            
 
             return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+           _db.Categories.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
