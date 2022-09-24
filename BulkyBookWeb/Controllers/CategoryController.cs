23408709e0b1fc8ce4e9409fb0ea6155882c408c
facundomarcos <facundomarcos@live.com.ar>
2022-09-24 00:00:47 -0300

    41

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -39,6 +39,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
 
                 return RedirectToAction("Index");
             }
@@ -78,6 +79,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
 
                 return RedirectToAction("Index");
             }
@@ -115,8 +117,8 @@ namespace BulkyBookWeb.Controllers
             }
                 _db.Categories.Remove(obj);
                 _db.SaveChanges();
-
-                return RedirectToAction("Index");
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
             
            
 
